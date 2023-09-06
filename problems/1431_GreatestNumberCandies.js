7641e9bb49cf6d15a241fab56fe9a66ed40410bc
Kaiwen Tsou <kktsou@ucsd.edu>
2023-09-06 13:49:47 -0700

    Start 1431

---
+++ problems/1431_GreatestNumberCandies.js
@@ -0,0 +1,10 @@
+// Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    
+};
